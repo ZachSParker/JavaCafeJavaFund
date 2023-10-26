b8985db894aade91b98027d1cbb1a4ad2437587f
Zach Parker <evergreenzach@gmail.com>
2023-10-26 00:45:04 -0600

    wrote first function for core assign

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main { 
+    public static void main(String[] args){
+}
+    public int getStreakGoal(int end){
+        int sum = 0;
+        for (int i = 1; i<end;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+    
+}
